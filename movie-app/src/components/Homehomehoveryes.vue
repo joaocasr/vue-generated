2eb158aac8979e30158f645e27c98b88a6b99501
joaocasr <castrojoao839@gmail.com>
2025-07-09 22:43:09 +0100

    movieapp

---
+++ movie-app/src/components/Homehomehoveryes.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+ <div class="grid-item-3367145 container3367146" v-on:click="gotoHomePage3367146()">
+  <p class="grid-item-3367145 text3367147">
+   HOME
+  </p>
+ </div>
+</div>
+
+</template>
+    
+<script>
+export default {
+
+    data(){
+        return {
+           
+        }
+    },
+    methods:{
+        gotoHomePage3367146(){
+            this.$router.push({path:"/homepage"});
+        }
+	}
+}
+</script>
+<style lang="css" scoped>
+@import '../assets/homehomehoveryes.css';
+</style>
---
+++ movie-app/src/components/Movie2.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+ <img alt="container3367259" class="grid-item-3367167 container3367259" src="/cloverfieldi753213367259.png"/>
+ <div class="grid-item-3367167 container3367260">
+  <p class="grid-item-3367167 text3367261">
+   Cloverfield
+  </p>
+  <p class="grid-item-3367167 text3367262">
+   1h,34min
+  </p>
+  <p class="grid-item-3367167 text3367263">
+   10 Oct 2021
+  </p>
+ </div>
+ <div class="grid-item-3367167 rectangle3367264">
+  <p class="grid-item-3367167 text3367265">
+   4.0
+  </p>
+ </div>
+ <v-rating :length="5" :model-value="starsSeletec3367266" :size="25" class="grid-item-3367167 srating3367266"  half-increments hover readonly >
+ </v-rating>
+</div>
+
+</template>
+    
+<script>
+export default {
+
+    data(){
+        return {
+        starsSeletec3367266:"3"   
+        }
+    }
+}
+</script>
+<style lang="css" scoped>
+@import '../assets/movie2.css';
+</style>
