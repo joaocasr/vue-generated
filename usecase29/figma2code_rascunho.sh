a8d4c53c67a44bb77d3a88a99f2a41195ac2b0a3
joaocasr <castrojoao839@gmail.com>
2025-07-09 22:30:53 +0100

    movies app generated

--- usecase29/figma2code_rascunho.sh
+++
@@ -1,8 +0,0 @@
-# Install project dependencies packages
-npm install vuetify
-npm install @mdi/font
-npm install
-
-#Run Vue project
-npm run build
-npm run preview
--- usecase9/figma2code.sh
+++
@@ -1,6 +0,0 @@
-# Install project dependencies packages
-npm install
-
-#Run Vue project
-npm run build
-npm run preview
